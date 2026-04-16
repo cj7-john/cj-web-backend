4561ae571d506e04b1a7e226672c5ca6bf9c71c5
cj7-john <cibangukaj@gmail.com>
2026-04-16 06:20:35 +0200

    Add /contact/ endpoint for Netlify form

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',                              views.quote_request, name='quote_request'),
-    path('success/',                      views.quote_success,  name='quote_success'),
-    path('invoice/<str:invoice_number>/', views.invoice_view,   name='invoice_view'),
+    # Contact/Quote form endpoint
+    path('contact/', views.quote_request, name='quote_request'),
+
+    # Success page
+    path('success/', views.quote_success, name='quote_success'),
+
+    # Invoice view
+    path('invoice/<str:invoice_number>/', views.invoice_view, name='invoice_view'),
 ]
